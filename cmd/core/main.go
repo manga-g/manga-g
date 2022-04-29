0e41021835afad6e933bb90dbf394027214fdb03
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-04-28 23:04:10 -0400

    working urasunday.com downloader

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -11,35 +11,33 @@ func main() {
 	MangaG := new(app.App)
 
 	//	mangaUrl := "https://somemangasite.com/{mangaid}/{pagenumber}"
-	fmt.Println("Starting MangaG...")
+	//fmt.Println("Starting MangaG...")
 	fmt.Println("Please Enter a URL for for a Manga's first page to download:")
 	MangaUrl := MangaG.GetInput()
 	fmt.Println("trying to grab manga from:", MangaUrl)
 
 	//	time.Sleep(time.Second * 8)
-	MangaG.SaveHtml(MangaUrl, "manga.html")
-	fmt.Println("Saved HTML")
+	//	MangaG.SaveHtml(MangaUrl, "manga.html")
+	//	fmt.Println("Saved HTML")
 
-	fmt.Println("Attempting to detect Manga From Site...")
-	html := MangaG.LoadHtml("manga.html")
-	fmt.Println("Attempting to load HTML from a file...")
+	//	fmt.Println("Attempting to detect Manga From Site...")
+	//	html := MangaG.LoadHtml("manga.html")
+	//	fmt.Println("Attempting to load HTML from a file...")
 
-	//	html := MangaG.StringifyHtml(MangaUrl)
+	html := MangaG.StringifyHtml(MangaUrl)
 	//	fmt.Println("Html was loaded into memory")
 
-	fmt.Println("Got title from header:\n" + MangaG.FindMangaTitle(html))
-	//	MangaG.NewDir("images")
-	//	ImageUrl, imgErr := MangaG.FindImageUrl(html)
-	//	if imgErr != nil {
-	//		fmt.Println("Error:", imgErr)
-	//	} else {
-	//		fmt.Println("Found Image URL:", ImageUrl[1])
-	//		MangaG.CycleImages(ImageUrl, MangaG.GetPageCount(html))
+	fmt.Println("Attempting to retrieve all manga pages from the site.")
+	//fmt.Println("Got title from header:\n" + MangaG.FindMangaTitle(html))
+	MangaG.NewDir("images")
+	// trim last two chars from found[1]
+	//found[1] = found[1][:len(found[1])-2]
+	//fmt.Println("found[1]:", found[1])
+	found := MangaG.FindImageUrls(html)
+	MangaG.DownloadAllWebp(found)
+	//MangaG.DownloadWebp(found[1], "images/001.webp")
 
-	//	fmt.Println("Attempting to retrieve all manga pages from the site.")
-	//cycleImages(MangaG, ImageUrl[1], MangaG.GetPageCount(html))
-	//	}
-	//	MangaG.DeleteFile("manga.html")
+	//MangaG.DeleteFile("manga.html")
 	//	fmt.Println("Deleted HTML no longer needed")
 	//MangaG.DeleteFile("images/")
 	//fmt.Println("Deleted images no longer needed")
