2fbc9e9d78911d99cc927abfa73892d48e0ad8f6
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-05-03 23:58:37 -0400

    fix: oops fixed it

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,48 +1,46 @@
 package main
 
 import (
-	"fmt"
-
-	"manga-g/app"
+    "manga-g/app"
 )
 
 // Entrypoint for the program.
 func main() {
-	MangaG := new(app.App)
+    MangaG := new(app.App)
 
-	MangaUrl := "https://mangadex.org/chapter/ef0a1ebe-c57d-49f2-bae8-75a64cfbd2a9/1"
-	//fmt.Println("Starting MangaG...")
-	//	fmt.Println("Please Enter a URL for a Manga's first page to download:")
-	//	MangaUrl := MangaG.GetInput()
-	//	fmt.Println("trying to grab manga from:", MangaUrl)
+    //MangaUrl := "https://mangadex.org/chapter/ef0a1ebe-c57d-49f2-bae8-75a64cfbd2a9/1"
+    //fmt.Println("Starting MangaG...")
+    //	fmt.Println("Please Enter a URL for a Manga's first page to download:")
+    //	MangaUrl := MangaG.GetInput()
+    //	fmt.Println("trying to grab manga from:", MangaUrl)
 
-	//time.Sleep(time.Second * 8)
-	//MangaG.SaveHtml(MangaUrl, "manga.html")
-	//fmt.Println("Saved HTML")
+    //time.Sleep(time.Second * 8)
+    //MangaG.SaveHtml(MangaUrl, "manga.html")
+    //fmt.Println("Saved HTML")
 
-	//fmt.Println("Attempting to detect Manga From Site...")
-	//html := MangaG.LoadHtml("manga.html")
-	//fmt.Println("Attempting to load HTML from a file...")
+    //fmt.Println("Attempting to detect Manga From Site...")
+    //html := MangaG.LoadHtml("manga.html")
+    //fmt.Println("Attempting to load HTML from a file...")
 
-	html := MangaG.StringifyHtml(MangaUrl)
-	fmt.Println(html)
-	//	fmt.Println("Html was loaded into memory")
+    //html := MangaG.StringifyHtml(MangaUrl)
+    //fmt.Println(html)
+    //	fmt.Println("Html was loaded into memory")
 
-	//	fmt.Println("Attempting to retrieve all manga pages from the site.")
-	MangaG.NewDir("images")
+    //	fmt.Println("Attempting to retrieve all manga pages from the site.")
+    MangaG.NewDir("images")
 
-	pages, _ := MangaG.GetMangaDex(html)
-	fmt.Println("Pages:", pages)
+    //pages, _ := MangaG.GetMangaDex(html)
+    //fmt.Println("Pages:", pages)
 
-	//fmt.Println("Manga pages were retrieved.", len(pages))
+    //fmt.Println("Manga pages were retrieved.", len(pages))
 
-	//fmt.Println("found[1]:", found[1])
-	//	found := MangaG.FindImageUrls(html)
-	//	MangaG.DownloadAllWebp(found)
-	//MangaG.DownloadWebp(found[1], "images/001.webp")
+    //fmt.Println("found[1]:", found[1])
+    //	found := MangaG.FindImageUrls(html)
+    //	MangaG.DownloadAllWebp(found)
+    //MangaG.DownloadWebp(found[1], "images/001.webp")
 
-	//MangaG.DeleteFile("manga.html")
-	//fmt.Println("Deleted HTML no longer needed")
-	//MangaG.DeleteFile("images/")
-	//fmt.Println("Deleted images no longer needed")
+    //MangaG.DeleteFile("manga.html")
+    //fmt.Println("Deleted HTML no longer needed")
+    //MangaG.DeleteFile("images/")
+    //fmt.Println("Deleted images no longer needed")
 }
