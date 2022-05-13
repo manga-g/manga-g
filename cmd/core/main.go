e826e480ccd2a2b54c9c4ca4e8af5ca713026769
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-05-13 18:08:55 -0400

    refactor: simple main.go

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,27 +1,6 @@
 package main
 
-import (
-	"fmt"
-
-	"manga-g/app"
-)
-
 // Entrypoint for the program.
 func main() {
-	MangaG := new(app.MG)
-	fmt.Println("Starting MangaG...")
-	if MangaG.Connected() {
-		fmt.Println("Please Enter a URL for a Manga's first page to download:")
-		MangaUrl := MangaG.GetInput()
-		if MangaG.ValidateUrl(MangaUrl) {
-			DoStuff(MangaG, MangaUrl)
-		} else {
-			fmt.Println("Invalid URL please try again.")
-			fmt.Println("Example: https://www.mangaeden.com/en/en-manga/one-piece/")
-			fmt.Println("Exiting...")
-		}
-	} else {
-		fmt.Println("Could not connect to the internet.")
-		fmt.Println("Exiting...")
-	}
+	InitMangaG()
 }
--- cmd/core/stuff.go
+++ cmd/core/stuff.go
@@ -6,6 +6,25 @@ import (
 	"manga-g/app"
 )
 
+func InitMangaG() {
+	MangaG := new(app.MG)
+	fmt.Println("Starting MangaG...")
+	if MangaG.Connected() {
+		fmt.Println("Please Enter a URL for a Manga's first page to download:")
+		MangaUrl := MangaG.GetInput()
+		if MangaG.ValidateUrl(MangaUrl) {
+			DoStuff(MangaG, MangaUrl)
+		} else {
+			fmt.Println("Invalid URL please try again.")
+			fmt.Println("Example: https://www.mangaeden.com/en/en-manga/one-piece/")
+			fmt.Println("Exiting...")
+		}
+	} else {
+		fmt.Println("Could not connect to the internet.")
+		fmt.Println("Exiting...")
+	}
+}
+
 func DoStuff(MangaG *app.MG, MangaUrl string) {
 	nameLimit := 15
 	//	fmt.Println("trying to grab manga from:", MangaUrl)
