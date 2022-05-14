260a814eff27346d6b348abb1aec9c01e5295ed1
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-05-13 23:20:35 -0400

    fix: input not working bug fix

--- ui/loop.go
+++ ui/loop.go
@@ -7,31 +7,74 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type Model struct{}
-
-func (m *Model) Init() tea.Cmd {
-	return nil
+type MangaModel struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
 }
 
-func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *MangaModel) Init() tea.Cmd {
+	return nil
+}
+func InitModel() tea.Model {
+	return &MangaModel{
+		choices:  []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},
+		selected: make(map[int]struct{}),
+	}
+
+}
+
+func (m *MangaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.Key:
+	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
 		case "ctrl+q":
 			return m, tea.Quit
+		case "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "enter":
+			if _, ok := m.selected[m.cursor]; ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
 		}
 	}
 	return m, nil
 }
 
-func (m *Model) View() string {
-	return "Ok"
+func (m *MangaModel) View() string {
+	footer := "\nPress ctrl+c to quit.\n"
+	header := "Enter a manga URL:\n"
+
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		header += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	header += footer
+	return header
 }
 
 func StartLoop() {
-	err := tea.NewProgram(&Model{}, tea.WithAltScreen()).Start()
+	program := tea.NewProgram(InitModel(), tea.WithAltScreen())
+	err := program.Start()
 	if err != nil {
 		_, err2 := fmt.Fprintf(os.Stderr, err.Error())
 		if err2 != nil {
@@ -39,5 +82,4 @@ func StartLoop() {
 		}
 		os.Exit(1)
 	}
-
 }
