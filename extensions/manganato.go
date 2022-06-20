b8f27cc686b9871bb4f6437f6c96187cafc63d1e
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-06-20 11:43:03 -0400

    fix comment

--- extensions/manganato.go
+++ extensions/manganato.go
@@ -1,38 +1,38 @@
 package extensions
 
 import (
-    "regexp"
-    "strings"
+	"regexp"
+	"strings"
 
-    "manga-g/app"
+	"manga-g/app"
 )
 
 const url = "https://manganato.com/search/story/"
 
 func NatoSearch(app *app.MG, query string) string {
 
-    html := app.StringifyHtml(url + query)
-    return html
+	html := app.StringifyHtml(url + query)
+	return html
 }
 
-// make a function to parse the html result to return only the div's with the class "search-story-item"
+// NatoParse a function to parse the html result to return only the div's with the class "search-story-item"
 func NatoParse(html string) []string {
-    var stories []string
-    rx := regexp.MustCompile("<div class=\"search-story-item\">.*?</div>")
+	var stories []string
+	rx := regexp.MustCompile("<div class=\"search-story-item\">.*?</div>")
 
-    for _, match := range rx.FindAllStringSubmatch(html, -1) {
-        match[0] = strings.ReplaceAll(match[0], "'", "")
-        match[0] = strings.ReplaceAll(match[0], "\"", "")
-        match[0] = strings.ReplaceAll(match[0], "\\", "")
-        match[0] = strings.ReplaceAll(match[0], " ", "")
-        match[0] = strings.ReplaceAll(match[0], "\n", "")
-        match[0] = strings.ReplaceAll(match[0], "\t", "")
-        match[0] = strings.ReplaceAll(match[0], "'", "")
-        match[0] = strings.ReplaceAll(match[0], "alt=", "")
-        match[0] = strings.ReplaceAll(match[0], ">", "")
-        //match[0] = match[0][:len(match[0])-2]
+	for _, match := range rx.FindAllStringSubmatch(html, -1) {
+		match[0] = strings.ReplaceAll(match[0], "'", "")
+		match[0] = strings.ReplaceAll(match[0], "\"", "")
+		match[0] = strings.ReplaceAll(match[0], "\\", "")
+		match[0] = strings.ReplaceAll(match[0], " ", "")
+		match[0] = strings.ReplaceAll(match[0], "\n", "")
+		match[0] = strings.ReplaceAll(match[0], "\t", "")
+		match[0] = strings.ReplaceAll(match[0], "'", "")
+		match[0] = strings.ReplaceAll(match[0], "alt=", "")
+		match[0] = strings.ReplaceAll(match[0], ">", "")
+		//match[0] = match[0][:len(match[0])-2]
 
-        stories = append(stories, match[0])
-    }
-    return stories
+		stories = append(stories, match[0])
+	}
+	return stories
 }
