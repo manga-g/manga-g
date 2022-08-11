c7ab410bc6d9e55f822bebc6075a7b1dec3d232b
Danya <d.2510086@gmail.com>
2022-08-11 22:43:48 +0200

    Added a message at the beginning of a program whether the server is working at the moment

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 
@@ -36,6 +38,13 @@ func main() {
 	//basedApiUrl := "http://localhost:" + port + "/"
 	basedApiUrl := "http://manga-api.bytecats.codes/"
 	mangaSaveDir := "./"
+	_, err := http.Get(basedApiUrl)
+	if err == nil {
+		fmt.Println("Online Manga is ready!")
+	} else {
+		fmt.Println("Online Manga is offline at the moment ;(\nBe back online ASAP =)")
+		os.Exit(1)
+	}
 	fmt.Print("Search for manga: ")
 	query := app.GetInput()
 	//if there is no input, loop the request 3 times
@@ -93,9 +102,10 @@ func main() {
 	var mangaChapters app.MangaChapters
 	app.ParseChapters(res, &mangaChapters)
 	var chapterTitles []string
-	for i, chapter := range mangaChapters.Chapters {
-		chapterTitles = append(chapterTitles, fmt.Sprintf("%d. %s", i+1, chapter))
-	}
+	//for i, chapter := range mangaChapters.Chapters {
+	//	chapterTitles = append(chapterTitles, fmt.Sprintf("%d. %s", i+1, chapter))
+	//}
+
 	for _, title := range chapterTitles {
 		fmt.Println(title)
 	}
