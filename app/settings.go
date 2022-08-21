d8e363d8734e37c695ea53984c109d9b4bfee5a2
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-08-21 19:42:32 -0400

    start of refactor of main to make room for new features easily

---
+++ app/settings.go
@@ -0,0 +1,27 @@
+package app
+
+func LoadSettings() {
+	//gotenv.Load()
+	//basedApiUrl := os.Getenv("BASED_API_URL")
+	//fmt.Println("BASED_API_URL:", basedApiUrl)
+	//if basedApiUrl == "" {
+	//    fmt.Println("BASED_API_URL is not set in env")
+	//    os.Exit(1)
+	//}
+
+	//mangaSaveDir := os.Getenv("MANGA_SAVE_DIR")
+	//fmt.Println("MANGA_SAVE_DIR:", mangaSaveDir)
+	//if mangaSaveDir == "" {
+	//    fmt.Println("MANGA_SAVE_DIR is not set in env")
+	//    currentDirectory, err := os.Getwd()
+	//    if err != nil {
+	//        fmt.Println("Error getting current directory:", err)
+	//        os.Exit(1)
+	//    }
+	//    fmt.Println("Using default" + currentDirectory)
+	//    mangaSaveDir = "."
+	//}
+
+	//port := "3000"
+	//basedApiUrl := "http://localhost:" + port + "/"
+}
