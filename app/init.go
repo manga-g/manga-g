712188a4014650bac490c86b49be015abaf78f48
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-09-10 20:03:18 -0400

    main menu loop with Back to menu feature

--- app/init.go
+++ app/init.go
@@ -2,35 +2,10 @@ package app
 
 import (
 	"fmt"
-	"net/url"
-	"os"
 )
 
 func Init(basedApiUrl string) {
 	CheckApi(basedApiUrl)
-	fmt.Println("Enter your choice: (1-3)")
-	fmt.Println("1.) Search Manga")
-	fmt.Println("2.) Search Comics")
-	fmt.Println("3.) Exit")
-	menuChoice := GetInput()
-	QueryCheck(menuChoice)
-	if menuChoice == "1" {
-		fmt.Print("Search for manga: ")
-		query := GetInput()
-		QueryCheck(query)
-		query = url.QueryEscape(query)
-		MkSearch(basedApiUrl, query)
-	}
-	if menuChoice == "2" {
-		fmt.Print("Search for comic: ")
-		query := GetInput()
-		QueryCheck(query)
-		query = url.QueryEscape(query)
-		ComicSearch()
-	}
-	if menuChoice == "3" {
-		os.Exit(0)
-	}
-
+	StartMenu(basedApiUrl)
 	fmt.Println("Done")
 }
---
+++ app/main-menu.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+)
+
+func DisplayOptions() {
+	fmt.Println("Enter your choice: (1-3)")
+	fmt.Println("1.) Search Manga")
+	fmt.Println("2.) Search Comics")
+	fmt.Println("3.) Exit")
+}
+func StartMenu(basedApiUrl string) {
+
+	for started := true; started == true; {
+		if !started {
+			break
+		}
+		DisplayOptions()
+		backToMenu := false
+		switch menuChoice := GetInput(); menuChoice {
+		case "1":
+			fmt.Print("Search for manga: ")
+			query := GetInput()
+			if query == "<" {
+				backToMenu = true
+			}
+			if query != "<" {
+				QueryCheck(query)
+				//query = url.QueryEscape(query)
+				MkSearch(basedApiUrl, query)
+				started = false
+			}
+
+		case "2":
+			fmt.Print("Search for comic: ")
+			query := GetInput()
+			if query == "<" {
+				backToMenu = true
+			}
+			if query != "<" {
+				QueryCheck(query)
+				query = url.QueryEscape(query)
+				ComicSearch()
+				started = false
+			}
+			if query == "<" {
+				backToMenu = true
+			}
+
+		case "3":
+			os.Exit(0)
+		default:
+			if backToMenu == false {
+				fmt.Println("Invalid Option")
+			}
+		}
+	}
+}
